bebc3427bbebae89e4d3b796d50dcdc1bbbe443b
Lawrence Peczkowski-Soto <lawpeczkowski@gmail.com>
2019-12-08 02:57:05 -0500

    replaced the starting app with home-app

--- apps/home-app/jest.config.js
+++ apps/home-app/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'the-flaming-ox',
+  name: 'home-app',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/the-flaming-ox',
+  coverageDirectory: '../../coverage/apps/home-app',
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js'
--- apps/home-app/src/app/app.component.spec.ts
+++ apps/home-app/src/app/app.component.spec.ts
@@ -14,18 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'the-flaming-ox'`, () => {
+  it(`should have as title 'home-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('the-flaming-ox');
+    expect(app.title).toEqual('home-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to the-flaming-ox!'
-    );
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to home-app!');
   });
 });
