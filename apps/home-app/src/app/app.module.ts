044f4015ffd0c256f793b33a308e48e4cce16c10
Lawrence Peczkowski-Soto <lawpeczkowski@gmail.com>
2019-12-21 00:06:28 -0500

    update tslint and fix linting error

--- apps/home-app/src/app/app.module.ts
+++ apps/home-app/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
-import { ClarityModule } from '@clr/angular';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
