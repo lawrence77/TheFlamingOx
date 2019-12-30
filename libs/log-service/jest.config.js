0d6c9fc05a3650a49d207abd9cc2b5119f4ecf5b
Lawrence Peczkowski-Soto <lawpeczkowski@gmail.com>
2019-12-29 21:51:40 -0500

    add tests to simple-console-service

--- libs/log-service/jest.config.js
+++ libs/log-service/jest.config.js
@@ -2,8 +2,4 @@ module.exports = {
   name: 'log-service',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/log-service',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
 };
--- libs/log-service/src/lib/Console/simple-console-service.spec.ts
+++ libs/log-service/src/lib/Console/simple-console-service.spec.ts
@@ -1,14 +1,73 @@
-describe('Console', () => {
+import { LogLevel } from '@shared';
 
- // TODO mock console.log, console.warn, and console.error
+import { Console } from './simple-console-service';
+
+describe('Console', () => {
+  const testArray = [false, 5, 'test', { innerObject: 'test' }, ['foo', 'bar'], undefined];
+
+  let testLog: jest.SpyInstance<void, [any?, ...any[]]>;
+  let testWarn: jest.SpyInstance<void, [any?, ...any[]]>;
+  let testError: jest.SpyInstance<void, [any?, ...any[]]>;
+
+  beforeEach(() => {
+    testLog = jest.spyOn(console, 'log');
+    testWarn = jest.spyOn(console, 'warn');
+    testError = jest.spyOn(console, 'error');
+
+    testLog.mockReset();
+    testWarn.mockReset();
+    testError.mockReset();
+  });
 
   it('should print everything when log level is set to ALL', () => {
+    const output = new Console(LogLevel.All);
+    expect(testLog.mock.calls.length).toBe(0);
 
-  })
+    output.log(LogLevel.Error, testArray[0]);
+    output.log(LogLevel.Warn, testArray[1]);
+    output.log(LogLevel.Info, testArray[2]);
+    output.log(LogLevel.Config, testArray[3]);
+    output.log(LogLevel.Trace, testArray[4]);
+    output.log(LogLevel.All, testArray[5]);
+
+    expect(testLog.mock.calls.length).toBe(4);
+    expect(testWarn.mock.calls.length).toBe(1);
+    expect(testError.mock.calls.length).toBe(1);
+  });
+
   it('should print nothing when log level is set to NONE', () => {
+    const output = new Console(LogLevel.None);
+    expect(testLog.mock.calls.length).toBe(0);
 
-  })
-  it('should print somethings when log level is set to INFO', () => {
+    output.log(LogLevel.Error, testArray[0]);
+    output.log(LogLevel.Warn, testArray[1]);
+    output.log(LogLevel.Info, testArray[2]);
+    output.log(LogLevel.Config, testArray[3]);
+    output.log(LogLevel.Trace, testArray[4]);
+    output.log(LogLevel.All, testArray[5]);
 
-  })
-})
+    expect(testLog.mock.calls.length).toBe(0);
+    expect(testWarn.mock.calls.length).toBe(0);
+    expect(testError.mock.calls.length).toBe(0);
+  });
+
+  it('should filter logs when log level is set to INFO', () => {
+    const output = new Console(LogLevel.Info);
+    expect(testLog.mock.calls.length).toBe(0);
+
+    output.log(LogLevel.Error, testArray[0]);
+    output.log(LogLevel.Warn, testArray[1]);
+    output.log(LogLevel.Info, testArray[2]);
+    output.log(LogLevel.Config, testArray[3]);
+    output.log(LogLevel.Trace, testArray[4]);
+    output.log(LogLevel.All, testArray[5]);
+
+    expect(testLog.mock.calls.length).toBe(1);
+    expect(testWarn.mock.calls.length).toBe(1);
+    expect(testError.mock.calls.length).toBe(1);
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+});
