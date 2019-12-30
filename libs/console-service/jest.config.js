78e27a02db9c50288a614f34721292fef8d60818
Lawrence Peczkowski-Soto <lawpeczkowski@gmail.com>
2019-12-29 21:14:10 -0500

    remove the console service

--- libs/console-service/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'console-service',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/console-service',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
